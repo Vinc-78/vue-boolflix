bce7b1ce30acacef8c6405cc0b7b8f333d2cede5
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2021-11-18 16:45:03 +0100

    completato milestone 3

--- src/components/SingolCard.vue
+++ src/components/SingolCard.vue
@@ -1,17 +1,26 @@
 <template>
 <div class="text-center">
 
+   
     <img :src="coverPath" alt="">
-
+    
    
 
-    <h3> <strong>Titolo: {{titolo}}</strong></h3>
+    <h3 class="py-3"> <strong>Titolo: {{titolo}}</strong></h3>
     <h4>({{titoloOriginale}})</h4>
     <img class="w-25" v-bind:src="require ( '@/assets/'+ lingua)" alt="">
-    <h5>Voto: {{voto}} </h5>
     <h5>Sigla Paese: {{paese}} </h5>
-
+    <h5>Voto: {{voto}}</h5>
+    <div class="py-3">
+    <i class="fa fa-star fs-3 text-danger" v-for="(el,i) in calcStar" :key="'A'+ i"></i>
+    <i class="fa fa-star-o fs-3 text-danger" v-for="(el,j) in (5 - calcStar)" :key="'B'+ j"></i>
+    </div>
     
+    
+    <!-- Promemoria per il ciclo v-for e il warning in console
+        
+        https://stackoverflow.com/questions/51086657/vue-warn-duplicate-keys-detected-x-this-may-cause-an-update-error
+     Same key for different v-for loops causing this warning.you can avoid this using different key for different v-for loops. -->
         
 </div>
 
@@ -33,6 +42,7 @@ export default {
     }, 
 
     computed:{
+
         coverPath() {
             const linkRoot = "https://image.tmdb.org/t/p/";
 
@@ -46,6 +56,15 @@ export default {
 
 
         },
+
+        calcStar(){
+
+            const numStar = Math.ceil((this.voto / 2));
+
+            return numStar;
+
+
+        }
     },
 
 
